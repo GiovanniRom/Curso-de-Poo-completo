59aff582559452c1b31b659c8beb8ecc7f71acd1
Giovanni Romero <garm9609@gmail.com>
2021-06-30 17:23:06 -0500

    Herencia multiple con iu

--- HerenciaMultiple/src/ico/fes/iu/MiVentana.java
+++ HerenciaMultiple/src/ico/fes/iu/MiVentana.java
@@ -5,10 +5,55 @@
  */
 package ico.fes.iu;
 
+import java.awt.HeadlessException;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import javax.swing.JFrame;
+
 /**
  *
  * @author cakit
  */
-public class MiVentana {
+public class MiVentana extends JFrame implements WindowListener{
+    public MiVentana() throws HeadlessException{
+    super("Mi ventana");
+    setSize(600,400);
+    setVisible(Boolean.TRUE);
+            addWindowListener(this);
+}
+
+    @Override
+    public void windowOpened(WindowEvent we) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowClosing(WindowEvent we) {
+        System.out.println("Cerrando ventana");    }
+
+    @Override
+    public void windowClosed(WindowEvent we) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowIconified(WindowEvent we) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent we) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowActivated(WindowEvent we) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent we) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
